7c6ba57adf6bd345d4f178a10e0b97b17e036f69
Shane Rosenthal <srosenthal82@gmail.com>
2026-05-11 16:06:15 -0400

    updates composer

--- resources/views/native/counter.blade.php
+++ resources/views/native/counter.blade.php
@@ -1,10 +1,10 @@
 <native:column class="w-full h-full items-center justify-center bg-white gap-4">
-    <native:text class="text-6xl font-bold text-orange-500">{{$count}}</native:text>
-    <native:row class="gap-4 justify-center">
-        <native:column @press="decrement" class="px-8 py-4 shadow-lg rounded-lg bg-red-400" center>
+    <native:text class="text-6xl font-bold text-orange-600">{{$count * 100}}</native:text>
+    <native:row class="gap-8 justify-center">
+        <native:column @press="decrement" class="px-8 py-4 shadow rounded bg-red-400" center>
             <native:text  class="text-center text-2xl text-white">👇</native:text>
         </native:column>
-        <native:column @press="increment" class="px-8 py-4 shadow-lg rounded-lg bg-blue-400" center>
+        <native:column @press="increment" class="px-8 py-4 shadow rounded bg-blue-400" center>
             <native:text class="text-center font-bold text-2xl text-white">👆</native:text>
         </native:column>
     </native:row>
